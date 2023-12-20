811aeb8a521fcda43f27ddabf36b759c8abcb469
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2023-12-20 10:39:50 +0530

    server creation

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const dotenv=require("dotenv");
+const cors= require("cors");
+const connection = require("./db");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+// setup the server
+
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(cookieParser());
+// set up the body parser for handling form data
+// submitted by the user
+app.use(bodyParser.urlencoded({ extended: false }));
+// set up the session
+
+app.get("/",(req,res) => {
+    res.send("Hellow Welcome to Express!");
+});
+
+// setup config path
+// dotenv.config({path:"config/config.env"}); // for env but path is inside the config env
+dotenv.config(); // for single env files in env path
+
+// start the server and listen the request
+
+app.listen(process.env.PORT,async() => {
+    try{
+      await connection();
+    }
+    catch(e){
+    console.log("error",e);
+    }
+     console.log(`Server is started ${process.env.PORT}`);
+});
